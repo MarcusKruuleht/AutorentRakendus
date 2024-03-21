4ea95981e71f3ecc7075af1234244b16789026fe
Steven Mirontšuk <steven.mirontsuk1@gmail.com>
2024-03-21 09:42:47 +0200

    Starting with homepage

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import Splash from './src/screens/Splash';
+import Home from './src/screens/Home';
 
 const App = () => {
     return (
         <SafeAreaView>
-            <Splash />
+            <Home />
         </SafeAreaView>
     );
 };
---
+++ src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+    </View>
+  );
+};
+
+export default Home;
